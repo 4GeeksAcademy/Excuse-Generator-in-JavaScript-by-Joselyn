d0d3ae6506f94a229fabf7fa5673b7e6999679f5
JoselynFio <fiorecortes47@gmail.com>
2026-01-16 19:01:27 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,33 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function whoToBlame(){
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const randomIndex = Math.floor( Math.random() * 3 );
+  return who[randomIndex]
+}
+
+function whatTheyDid(){
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  const randomIndex = Math.floor( Math.random() * 3);
+  return action[randomIndex]
+}
+
+function whatToAvoid(){
+  let what = ['my homework', 'my phone', 'the car'];
+  const randomIndex = Math.floor( Math.random() * 2);
+  return what[randomIndex]
+}
+
+function when(){
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  const randomIndex = Math.floor( Math.random() * 4);
+  return when[randomIndex]
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const h2Element = document.getElementById('excuse')
+  
+  h2Element.innerHTML =  `${whoToBlame()} ${whatTheyDid()} ${whatToAvoid()} ${when()}`
 };
+
